fd13761153f2f2fa527500783429e86262ee4856
Deepali07ds <dummy@gmail.com>
2026-01-29 12:29:51 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/entity/Patient.java
@@ -0,0 +1,14 @@
+package com.edutech.progressive.entity;
+
+import java.util.Date;
+
+public class Patient {
+    private int patientId;
+    private String fullName;
+    private Date dateOfBirth;
+    private String contactNumber;
+    private String email;
+    
+    
+
+}
