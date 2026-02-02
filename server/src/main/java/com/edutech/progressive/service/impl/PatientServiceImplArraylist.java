bd1de2f0e624901b12e934766ae293765ccbb0ec
Deepali07ds <dummy@gmail.com>
2026-02-02 16:49:03 +0000

    Day 5 Task

--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplArraylist.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.edutech.progressive.entity.Patient;
 import com.edutech.progressive.service.PatientService;
 
+@Service
 public class PatientServiceImplArraylist implements PatientService {
     private static List<Patient> patientList = new ArrayList<>();
     @Override
@@ -16,18 +19,22 @@ public class PatientServiceImplArraylist implements PatientService {
 
     @Override
     public Integer addPatient(Patient patient) {
-        patientList.add(patient);
-        return patient.getPatientId();
+        if (patientList.add(patient)) {
+            return 1;
+        }
+        else return -1;
     }
 
     @Override
     public List<Patient> getAllPatientSortedByName() {
-        Collections.sort(patientList);
-        return patientList;
+        List<Patient> list=new ArrayList<>(patientList);
+        Collections.sort(list);
+        return list;
     }
 
     @Override
-    public void emptyArrayList() {
+    public void emptyArrayList() throws Exception {
+        patientList.clear();
     }
 
 }
--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplJpa.java
@@ -1,5 +1,38 @@
 package com.edutech.progressive.service.impl;
 
-public class PatientServiceImplJpa  {
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Patient;
+import com.edutech.progressive.repository.PatientRepository;
+import com.edutech.progressive.service.PatientService;
+
+@Service
+public class PatientServiceImplJpa implements PatientService {
+    private final PatientRepository patientRepository;
+    public PatientServiceImplJpa(PatientRepository patientRepository){
+        this.patientRepository=patientRepository;
+    }
+
+    @Override
+    public List<Patient> getAllPatients() throws Exception {
+        return patientRepository.findAll();
+    }
+
+    @Override
+    public Integer addPatient(Patient patient) throws Exception {
+        Patient p = patientRepository.save(patient);
+        return p.getPatientId();
+    }
+
+    @Override
+    public List<Patient> getAllPatientSortedByName() throws Exception {
+        List<Patient> patients = patientRepository.findAll();
+        Collections.sort(patients);
+        return patients;
+    }
+    
 
 }
