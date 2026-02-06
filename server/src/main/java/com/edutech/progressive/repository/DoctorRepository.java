144f6c2c861f0ca701234fc417ee6c96a8acd017
Deepali07ds <dummy@gmail.com>
2026-02-06 12:17:53 +0000

    Day 7 Task

--- server/src/main/java/com/edutech/progressive/repository/DoctorRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/DoctorRepository.java
@@ -1,10 +1,15 @@
 package com.edutech.progressive.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Doctor;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor,Integer>{
+    @Query("select d from Doctor d where d.doctorId=?1")
+    Optional<Doctor> findByDoctorId(int doctorId);
 }
